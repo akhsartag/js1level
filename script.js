cd343124152edeb233b8f4afbc0da7e2473dbe24
ahsario <amaru-gevara@mail.ru>
2021-01-21 12:45:56 +0300

    task of first lesson done

---
+++ script.js
@@ -0,0 +1,20 @@
+var Tc = +prompt("Введи градусы по Цельсию!","36"),
+    Tf = (9/5)*Tc+32;
+
+alert(Tf);
+
+var admin, name;
+name = "Василий";
+admin = name;
+
+alert(admin);
+
+var a = +prompt("enter A","32"),
+    b = +prompt("enter B","48");
+
+a += b;
+b = a - b;
+a -= b;
+
+alert (`A = ${a}, B = ${b}`);
+
