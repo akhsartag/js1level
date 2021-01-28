3368bb61ca62412ad3ca832ecf88ebcbd7ffd96d
ahsario <amaru-gevara@mail.ru>
2021-01-28 15:12:07 +0300

    tasks of lesson three done

--- script.js
+++ script.js
@@ -1,26 +1,61 @@
 // 1
+let i = 0;
+while (i <= 100) {
+	let sum = 0;
+	for (let j = 2; j <= i/2+1; j++) {
+		if (i % j == 0) {
+			sum++;
+		}
+	}
+	if (sum == 0) {
+		console.log(i)
+	}
+	i++;
+}
 
-var Tc = +prompt("Введи градусы по Цельсию!","36"),
-    Tf = (9/5)*Tc+32;
+//2
+let basketPrice = [
+	{
+		title:'qwe',
+		price: 100
+	},
+	{
+		title:'qhwe',
+		price: 200
+	},
+	{
+		title:'qjwe',
+		price: 300
+	},
+	{
+		title:'qwke',
+		price: 400
+	}
+]
 
-alert(Tf);
+for (let item of basketPrice) {
+	console.log(`товар ${item.title} стоит ${item.price}`)
+}
 
-// 2
+function countBasketPrice(mas){
+	let res = 0;
+	for (let item of basketPrice) {
+		res += item.price;
+	}
+	return res;
+}
 
-var admin, name;
-name = "Василий";
-admin = name;
+console.log(countBasketPrice(basketPrice));
 
-alert(admin);
+//4
 
-// *
+for (let i = 0; i <= 9; console.log(i++)){}
 
-var a = +prompt("enter A","32"),
-    b = +prompt("enter B","48");
+//5
 
-a += b;
-b = a - b;
-a -= b;
-
-alert (`A = ${a}, B = ${b}`);
-
+for (let i = 1; i <= 20; i++) {
+	let arr = new Array(i);
+	arr.fill('x');
+	let res = arr.join('');
+	console.log(res)
+}
