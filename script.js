375a37e036f16f80e3484c5a820515fae6a90ce3
ahsario <amaru-gevara@mail.ru>
2021-01-31 23:53:28 +0300

    lesson 4 is done

--- script.js
+++ script.js
@@ -1,61 +1,14 @@
-// 1
-let i = 0;
-while (i <= 100) {
-	let sum = 0;
-	for (let j = 2; j <= i/2+1; j++) {
-		if (i % j == 0) {
-			sum++;
-		}
+let num = +prompt("enter number", "837");
+let numberObj = num => {
+	
+	if (num >= 0 && num <= 999) {
+		let obj = {"единицы": num % 10, "десятки": Math.trunc(num % 100 / 10), "сотни": Math.trunc(num / 100)};
+		return obj;
+	} else {
+		console.error("Out of range");
+		return {}
 	}
-	if (sum == 0) {
-		console.log(i)
-	}
-	i++;
+	
 }
 
-//2
-let basketPrice = [
-	{
-		title:'qwe',
-		price: 100
-	},
-	{
-		title:'qhwe',
-		price: 200
-	},
-	{
-		title:'qjwe',
-		price: 300
-	},
-	{
-		title:'qwke',
-		price: 400
-	}
-]
-
-for (let item of basketPrice) {
-	console.log(`товар ${item.title} стоит ${item.price}`)
-}
-
-function countBasketPrice(mas){
-	let res = 0;
-	for (let item of basketPrice) {
-		res += item.price;
-	}
-	return res;
-}
-
-console.log(countBasketPrice(basketPrice));
-
-//4
-
-for (let i = 0; i <= 9; console.log(i++)){}
-
-//5
-
-for (let i = 1; i <= 20; i++) {
-	let arr = new Array(i);
-	arr.fill('x');
-	let res = arr.join('');
-	console.log(res)
-}
+console.log(numberObj(num));
